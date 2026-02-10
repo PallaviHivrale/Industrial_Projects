630ab2a2959bcd5e5fb2f335e22c976a1cb8d8ca
Pallavi Hivrale <hivralepallavi2004@gmail.com>
2026-02-11 00:15:02 +0530

    Add files via upload

--- Packer_Unpacker/Packer.java
+++ Packer_Unpacker/Packer.java
@@ -1,1 +1,102 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Project Name  : File Packer
+//  Author        : Pallavi Hivrale
+//  Description   : This program reads all .txt files from a given folder,
+//                  creates a pack file, writes a 100-byte header for each
+//                  file, encrypts file data using XOR key, and stores it.
+//  Date          : 30 / 01 / 2026
+//
+/////////////////////////////////////////////////////////////////////////////
 
+import java.io.*;
+import java.util.*;
+
+class Packer
+{
+    public static void main(String A[]) throws Exception
+    {
+        String Header = null;
+
+        byte Key = 0x11;
+
+        int iRet = 0;
+        int i = 0, j =0 ;
+
+        byte Buffer[] = new byte[1024];
+
+        byte bHeader[] = new byte[100];
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of folder : ");
+        String FolderName = sobj.nextLine();
+
+        System.out.println("Enter the name of packed file : ");
+        String PackName = sobj.nextLine();
+        
+        File fobj = new File(FolderName);
+
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            File PackObj = new File(PackName);
+
+            PackObj.createNewFile();
+
+            FileOutputStream foobj = new FileOutputStream(PackObj);
+
+            FileInputStream fiobj = null;
+
+            System.out.println("Folder is present"); 
+
+            File fArr[] = fobj.listFiles();
+
+            System.out.println("Number of files in the folder are : "+fArr.length);
+
+            for(i = 0; i < fArr.length; i++)
+            {
+                fiobj = new FileInputStream(fArr[i]);
+
+                if(fArr[i].getName().endsWith(".txt"))
+                {
+                    // ---------------- HEADER FORMATION ----------------
+                    Header = fArr[i].getName() + " " + fArr[i].length();
+
+                    for(j = Header.length(); j < 100; j++)
+                    {
+                        Header = Header + " ";
+                    }
+                    
+                    bHeader = Header.getBytes();
+
+                    // Write header into pack file
+                    foobj.write(bHeader,0,100);
+
+                    // ---------------- FILE DATA COPY ----------------
+                    
+                    // Read the data from input files from Marvellous folder
+                    while((iRet = fiobj.read(Buffer)) != -1)
+                    {
+                        // Encryption Logic 
+                        for(j = 0; j < iRet ; j++)
+                        {
+                            Buffer[j] = (byte)(Buffer[j] ^ Key);
+                        }
+
+                        // Write the files data into pack file
+                        foobj.write(Buffer,0,iRet);
+                    }
+                }
+
+                fiobj.close();
+            }
+
+            foobj.close();
+        }
+        else
+        {
+            System.out.println("There is no such folder");
+        }
+
+    }
+}
